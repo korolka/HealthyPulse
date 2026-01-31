c668a3ddff2a71aa0f1a34962b0077630a6a672c
Vlad <71081067+korolka@users.noreply.github.com>
2026-01-31 19:49:34 +0200

    feat: add initial structure of project (#1)
    * feat: add list of api features and main part of .net project.
    
    * feat: add new controllers and endpoints
    
    This commit has new controller: Doctor, Equipment, Measurement, MeasurementType, Patient. Also I add new endpoints almost for all: Create, Update, Delete, Get.
    
    * refactor: add csproj and solution file

---
+++ HealthyPulse/Controllers/MeasurementTypeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyPulse.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MeasurementTypeController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Ok();
+        }
+
+        [HttpPut("id")]
+        public IActionResult Update(int id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("id")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ HealthyPulse/Controllers/PatientController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyPulse.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PatientController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Ok();
+        }
+
+        [HttpPut("id")]
+        public IActionResult Update(int id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("id")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+
+    }
+}
