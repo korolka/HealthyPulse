3a00ea98c7949395ea17ca796188450c2891e69e
korolka <korolka212@gmail.com>
2026-02-17 23:27:13 +0200

    feat: configure doctor controller Created new doctor model. Added documentation and example values to Create and Get endpoints.

--- HealthyPulse/Controllers/DoctorController.cs
+++ HealthyPulse/Controllers/DoctorController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HealthyPulse.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
 
 namespace HealthyPulse.Controllers
 {
@@ -6,12 +8,20 @@ namespace HealthyPulse.Controllers
     [Route("api/[controller]")]
     public class DoctorController : Controller
     {
+        /// <summary>
+        /// Create new doctor
+        /// </summary>
+        /// <param name="doctor">Doctor entity</param>
         [HttpPost]
-        public IActionResult Create()//record
+        public IActionResult Create([FromBody] Doctor doctor)//record
         {
             return Ok();
         }
 
+        /// <summary>
+        /// Get doctor by id
+        /// </summary>
+        /// <param name="id">Doctor id</param>
         [HttpGet("id")]
         public IActionResult Get(int id)
         {
