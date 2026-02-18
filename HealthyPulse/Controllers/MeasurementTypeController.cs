111bf2fb19e1c01ad4cb3337152982d81e9219fa
korolka <korolka212@gmail.com>
2026-02-18 23:32:01 +0200

    refactor: set up equipment, patient, measurement, measurementType controllers and create models.

--- HealthyPulse/Controllers/MeasurementTypeController.cs
+++ HealthyPulse/Controllers/MeasurementTypeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HealthyPulse.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HealthyPulse.Controllers
 {
@@ -6,18 +7,30 @@ namespace HealthyPulse.Controllers
     [Route("api/[controller]")]
     public class MeasurementTypeController : Controller
     {
+        /// <summary>
+        /// Create new measurementType 
+        /// </summary>
+        /// <param name="measurementType">MeasurementType entity</param>
         [HttpPost]
-        public IActionResult Create()
+        public IActionResult Create([FromBody] MeasurementType measurementType)
         {
             return Ok();
         }
 
+        /// <summary>
+        /// Update existing measurementType 
+        /// </summary>
+        /// <param name="id">MeasurementType id</param>
         [HttpPut("id")]
         public IActionResult Update(int id)
         {
             return Ok();
         }
 
+        /// <summary>
+        /// Delete measurementType 
+        /// </summary>
+        /// <param name="id">MeasurementType id</param>
         [HttpDelete("id")]
         public IActionResult Delete(int id)
         {
---
+++ HealthyPulse/Models/Patient.cs
@@ -0,0 +1,24 @@
+﻿namespace HealthyPulse.Models
+{
+    public class Patient
+    {
+
+        /// <summary>
+        /// Patient name
+        /// </summary>
+        /// <example>Andrei</example>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Patient last name
+        /// </summary>
+        /// <example>Martseniuk</example>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Patient`s date of birth
+        /// </summary>
+        /// <example>1984-01-10T20:45:34.910Z</example>
+        public DateTime DateOfBirth { get; set; }
+    }
+}
